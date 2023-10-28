802a8cb637ae2340029237d52a149210da005d1b
Yannis Krämer <yannkra21@gmail.com>
2023-10-28 11:14:08 +0200

    Implement genre dropdown.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 d3.csv("data/movies-originalDataset.csv", function() {
     console.log("data loaded")
-    // tree view after income
-
-
-    
+    // fill genre drop downs
+    genres = []
+    data.forEach(d => {
+        if (d.genre) {
+            d.genre.split(",").forEach(g => {
+                if (!genres.includes(g)) {
+                    genres.push(g);
+                }
+            });
+        }
+    });
+    genres.sort();
+    console.log("genres: ", genres);
+    const genreDropdowns = document.querySelectorAll('.genreSelection');
+    genreDropdowns.forEach((dropdown) => {
+        genres.forEach((genre) => {
+            const option = document.createElement('option');
+            option.value = genre;
+            option.textContent = genre;
+            dropdown.appendChild(option);
+        });
+    });
     // line plot releases per year
 
 
